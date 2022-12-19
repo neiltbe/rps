f7c1001cc7cfb48e375d15997cd3d21c06cc1b54
Neil Bhasin <nbhasin7@yahoo.com>
2022-12-18 19:16:17 -0500

    working

--- script.js
+++ script.js
@@ -8,26 +8,47 @@ else if (rng == 1)
 else 
     return "Scissors"
 }
+const score = {
+    player: 0,
+    computer: 0
+};
 let computerChoice = getComputerChoice();
 let playerChoice = "Scissors";
 playerChoice = playerChoice.toUpperCase()
 function playRound(player, computer){
     if (player == "ROCK" && computer == "Paper"){
+        score.computer++;
         return "You Lose!"
     }
     else if (player == "ROCK" && computer == "Scissors"){
+        score.player++;
         return "You win!"
     }
     else if (player == "SCISSORS" && computer == "Rock"){
+        score.computer++;
         return "You Lose!"
     }
     else if (player == "SCISSORS" && computer == "Paper"){
+        score.player++;
         return "You Win!"
     }
     else {
         return "It's a draw!"
     }
 }
-console.log(`Player: ${playerChoice}`)
-console.log(`Computer: ${computerChoice}`)
-console.log(playRound(playerChoice, computerChoice))
+function game(){
+    for (let i = 0; i < 5; i++){
+       let result = playRound(playerChoice, getComputerChoice());
+       console.log(result);
+    }
+    if (score.player > score.computer){
+        console.log(`"Player Wins with ${score.player} out of 5"`)
+    }
+    else if (score.player < score.computer){
+        console.log(`"Computer Wins with ${score.computer} out of 5"`)
+    }
+    else {
+        console.log(`"It's a draw with both computer scoring: ${score.computer} and player scoring: ${score.player}"`)
+    }
+    }
+game();
